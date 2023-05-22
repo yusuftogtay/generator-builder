934bc25a978a22e257ce96e6a4b8ee0a63b95261
yusuftogtay <yusuf.togtay@gmail.com>
2023-05-22 14:33:32 +0300

    Edit namespaces and publish locale added

--- src/Commands/GeneratorBuilderRoutesPublisherCommand.php
+++ src/Commands/GeneratorBuilderRoutesPublisherCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace InfyOm\GeneratorBuilder\Commands;
+namespace YusufTogtay\GeneratorBuilder\Commands;
 
 use File;
 use Illuminate\Console\Command;
@@ -13,7 +13,7 @@ class GeneratorBuilderRoutesPublisherCommand extends Command
      *
      * @var string
      */
-    protected $name = 'infyom.publish:generator-builder';
+    protected $name = 'yusuftogtay.publish:generator-builder';
 
     /**
      * The console command description.
@@ -31,14 +31,16 @@ class GeneratorBuilderRoutesPublisherCommand extends Command
     {
         if ($this->option('views')) {
             $this->publishViews();
+            $this->publishLocales();
         } else {
             $this->publishRoutes();
+            $this->publishLocales();
         }
     }
 
     private function publishRoutes()
     {
-        $path = config('infyom.laravel_generator.path.routes', app_path('Http/routes.php'));
+        $path = config('yusuftogtay.laravel_generator.path.routes', app_path('Http/routes.php'));
 
         $routeContents = file_get_contents($path);
 
@@ -55,7 +57,7 @@ class GeneratorBuilderRoutesPublisherCommand extends Command
     public function publishViews()
     {
         $sourceDir = __DIR__.'/../../views/';
-        $destinationDir = base_path('resources/views/infyom/generator-builder/');
+        $destinationDir = base_path('resources/views/yusuftogtay/generator-builder/');
 
         if (file_exists($destinationDir)) {
             $answer = $this->ask('Do you want to overwrite generator-builder? (y|N) :', false);
@@ -70,6 +72,30 @@ class GeneratorBuilderRoutesPublisherCommand extends Command
         File::copyDirectory($sourceDir, $destinationDir);
 
         $this->comment('generator-builder views published');
+        $this->info($destinationDir);
+    }
+
+    /**
+     * Publishes locales.
+     */
+    public function publishLocales()
+    {
+        $sourceDir = __DIR__.'/../../resources/lang/';
+        $destinationDir = base_path('resources/lang/');
+
+        if (file_exists($destinationDir)) {
+            $answer = $this->ask('Do you want to overwrite generator-builder locales? (y|N) :', false);
+
+            if (strtolower($answer) != 'y' and strtolower($answer) != 'yes') {
+                return false;
+            }
+        } else {
+            File::makeDirectory($destinationDir, 493, true);
+        }
+
+        File::copyDirectory($sourceDir, $destinationDir);
+
+        $this->comment('generator-builder locales published');
         $this->info($destinationDir);
     }
 
